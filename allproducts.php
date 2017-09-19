e77c26e62e206175646f22ce30efc07cd73578cc
Youjin Kim <ujlena@gmail.com>
2017-09-19 12:12:38 -0400

    add json file and logic file

---
+++ allproducts.php
@@ -0,0 +1,9 @@
+<?php
+
+$productsJson = file_get_contents("products.json");
+
+#var_dump($productsJson); 
+
+$productsArr = json_decode($productsJson, true);
+
+var_dump($productsArr);
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php require("allproducts.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
