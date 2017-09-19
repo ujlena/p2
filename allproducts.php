4d5052f2591dfc95f10753f5d55a37f3455b0189
Youjin Kim <ujlena@gmail.com>
2017-09-19 18:22:48 -0400

    display all products from json, add form action

--- allproducts.php
+++ allproducts.php
@@ -4,6 +4,72 @@ $productsJson = file_get_contents("products.json");
 
 #var_dump($productsJson); 
 
-#$productsArr = json_decode($productsJson, true);
+$productsArr = json_decode($productsJson, true);
 
 #var_dump($productsArr);
+
+
+#$producttypes = $_GET["producttypes"];
+#echo "You want to search for: " .$producttypesvalue;
+
+#$skintypevalue = $_GET["skintype"];
+#echo "Skin Type Selected: " .$skintypevalue;
+
+#$pricerange = $_GET["pricerange"];
+#echo "Price you chose: " .$pricerange;
+
+
+
+if (isset($_GET["producttypes"])) {
+	$producttypes = $_GET["producttypes"];
+	echo "You want to search for: " .$producttypes;
+}
+
+if (isset($_GET["skintype"])) {
+	$skintype = $_GET["skintype"];
+	echo "Skin Type Selected: " .$skintype;
+}
+
+if (isset($_GET["pricerange"])) {
+	$pricerange = $_GET["pricerange"];
+	echo "Price you chose: " .$pricerange;
+}
+
+
+if ($producttypes == "cleansers") {
+	$cleansers = "SELECTED";
+} else if ($producttypes == "toners") {
+	$toners = "SELECTED";
+} else if ($producttypes == "moisturizers") {
+	$moisturizers == "SELECTED";
+} else if ($producttypes = "eyecreams") {
+	$eyecreams = "SELECTED";
+}
+
+
+
+if ($skintype == "dry") {
+	$dry = "SELECTED";
+} else if ($skintype == "oily") {
+	$oily = "SELECTED";
+} else if ($skintype == "combination") {
+	$combination = "SELECTED";
+} else if ($skintype == "normal") {
+	$normal = "SELECTED";
+}
+
+/*
+if (($pricerange > 10) && ($pricerance < 25)) {
+	echo "10~25";
+} else if ($pricerange >= 25) && ($pricerance < 50) {
+	echo "25~50";
+}*/
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -19,29 +19,30 @@
 				<span>Choose category</span>
 			</label>
 			<select name="producttypes">
-				<option value="cleansers">Cleansers</option>
-				<option value="toners">Toners</option>
-				<option value="moisturizers">Moisturizers</option>
-				<option value="eyecreams">Eye Creams</option>
+				<option value="cleansers" <?=$cleansers?> >Cleansers</option>
+				<option value="toners" <?=$toners?> >Toners</option>
+				<option value="moisturizers"<?=$moisturizers?> >Moisturizers</option>
+				<option value="eyecreams" <?=$eyecreams?> >Eye Creams</option>
 			</select>
 		</p>
 
+		
 		<span>Select your skin type</span>
 		<!--input type: radio-->
 		<p>
-			<input type="radio" id="dry" name="skintype" value="dry">
+			<input type="radio" id="dry" name="skintype" value="dry" <?=$dry?> >
 			<label for="dry">Dry</label>
 		</p>
 		<p>	
-			<input type="radio" id="oily" name="skintype" value="oily">
+			<input type="radio" id="oily" name="skintype" value="oily" <?=$oily?> >
 			<label for="oily">Oily</label>
 		</p>
 		<p>	
-			<input type="radio" id="combination" name="skintype" value="combination">
+			<input type="radio" id="combination" name="skintype" value="combination" <?=$combination?> >
 			<label for="combination">Combination</label>
 		</p>
 		<p>
-			<input type="radio" id="normal" name="skintype" value="normal">
+			<input type="radio" id="normal" name="skintype" value="normal" <?=$normal?> >
 			<label for="normal">Normal</label>
 		</p>
 		
@@ -58,6 +59,49 @@
 			<input type="submit" class="btn" value="Find one for me">
 		</p>
 	</form>
+
+
+	<h3>Product matches you .. </h3>
+	<?php foreach ($productsArr as $ptype => $products) : ?>
+		<?php if ($producttypes == $ptype) : ?>
+			<h3>hello: <?=$ptype?></h3>
+
+			<?php foreach ($products as $index => $item) : ?>
+				<?php if ($skintype == $products[$index]["skintype"]) : ?>
+
+					<h3>you got it: <?=$products[$index]["skintype"]?></h3>
+
+				<?php endif; ?>
+
+			<?php endforeach; ?>
+			
+		<?php endif; ?>
+	<?php endforeach; ?>
+
+	
+
+
+	<?php foreach ($productsArr as $ptype => $products) : ?>
+		<div class="product">
+			<h2>Product Type: <?=$ptype?></h2>
+		
+			<?php foreach ($products as $index => $item) : ?>
+					
+					<div class="item">
+						<h3>Brand: <?=$products[$index]["brand"]?></h3>
+						<h3>Name: <?=$products[$index]["name"]?></h3>
+						<h3>Where to buy: <a href="<?=$products[$index]['url']?>">link to official website</a>
+						</h3>
+						<h3>Price: <?=$products[$index]["price"]?></h3>
+						<h3>Skin Type: <?=$products[$index]["skintype"]?></h3>
+					</div>
+
+			<?php endforeach; ?>
+		</div>
+	<?php endforeach; ?>
+
+
+
 
 </body>
 </html>
